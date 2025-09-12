723f0bdd8a9f6c9fa0cfac5d146b4d1c615b0cd6
brown-sung <brown.sung@kakaohealthcare.com>
2025-09-12 09:46:28 +0900

    타임아웃 수정

--- services.js
+++ services.js
@@ -184,7 +184,7 @@ async function callGeminiWithApiKey(
   context,
   modelName,
   isJson = false,
-  timeout = 25000
+  timeout = 40000
 ) {
   const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
   if (!GEMINI_API_KEY) {
